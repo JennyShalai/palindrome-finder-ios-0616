ed2bd1b0f28ddcbccc1cffc3fa09a3070f9dc47f
JennyShalai <jenny.shalai@gmail.com>
2016-06-11 13:36:05 -0400

    init

---
+++ objc-palindrome-inspector-ios-0616/objc-palindrome-inspector-ios-0616/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-palindrome-inspector-ios-0616
+//
+//  Created by Jenny on 6/11/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-palindrome-inspector-ios-0616/objc-palindrome-inspector-ios-0616/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc-palindrome-inspector-ios-0616
+//
+//  Created by Jenny on 6/11/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
